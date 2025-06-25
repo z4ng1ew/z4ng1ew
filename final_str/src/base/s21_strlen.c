bbafb9bc51b579af7b0c7f33226183466ddfab1e
z4ng1ew <z4ng1ew@gmail.com>
2025-06-25 20:48:53 +0300

    1

--- final_str/src/base/s21_strlen.c
+++
@@ -1,9 +0,0 @@
-#include "../s21_string.h"
-
-s21_size_t s21_strlen(const char *str) {
-  s21_size_t str_length = 0;
-  while (str[str_length] != '\0') {
-    str_length++;
-  }
-  return str_length;
-}
